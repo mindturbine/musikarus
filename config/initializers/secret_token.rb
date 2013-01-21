27cb2f03e33ad5d5edf6f90d2aec1bf30ffa3b6c
Gab Med <mindturbinellc@gmail.com>
2013-01-20 23:21:03 -0500

    created a rails app and status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Musikarus::Application.config.secret_token = '5cd20f423c32c2eb0917cafeabb35eb4a5be79e36c48f8b9cec5c49d3e28efe7c1681dd8ddcb065fad1e0c98670eb15290c81cd676b82d044336dcf666186317'
